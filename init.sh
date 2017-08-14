359af2d9a9e923701d706fbc6f710c67d270c171
ForStepik <e.ts.14@mail.ru>
2017-08-15 00:52:18 +0300

    1.8.12---

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
+sudo ln -sf /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
 sudo /etc/init.d/nginx start
