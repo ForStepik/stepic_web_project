1fdf8ee22d53ccb3bc23455e788a6dc529df8798
ForStepik <e.ts.14@mail.ru>
2017-08-17 02:11:32 +0300

    1.9.11

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #sudo mv ~/web/public/hello.py /home/box
+git clone https://github.com/ForStepik/stepic_web_project /home/box/web
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo gunicorn -b 0.0.0.0:8080 hello:app
-sudo /etc/init.d/nginx restart
+/home/box/web sudo gunicorn -b 0.0.0.0:8080 hello:app
+sudo /etc/init.d/nginx start
