72fbf5a5089807b94e112e00c4d79e276a8b1d78
ForStepik <e.ts.14@mail.ru>
2017-08-17 02:47:22 +0300

    1.9.11

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #sudo mv ~/web/public/hello.py /home/box/etc
 #sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo gunicorn -d 0.0.0.0:8080 hello:app &
+sudo gunicorn -b 0.0.0.0:8080 hello:app &
 sudo /etc/init.d/nginx start
