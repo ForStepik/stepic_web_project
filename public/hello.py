4f10b091247e0f6a76e8f0e53d1033c5527f32d7
ForStepik <e.ts.14@mail.ru>
2017-08-17 03:08:32 +0300

    1.11.9

--- public/hello.py
+++ public/hello.py
@@ -1,4 +1,10 @@
-bind "0.0.0.0:8080"
-working_dir: /home/box/web
-hello:app
-workers = 4
+CONFIG = {
+	'mode': 'wsgi',
+	'working_dir': '/home/box/web/hello.py',
+	'args': (
+		'--bind=0.0.0.0::8080',
+		'hello.app',
+		'--workers=4',
+		'hello:app'	
+	),
+}
