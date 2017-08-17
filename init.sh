bd10f3043fb1c2ce722e38b15a3953cffc1e5690
ForStepik <e.ts.14@mail.ru>
2017-08-17 04:50:32 +0300

    1.9.11

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo mkdir ~/etc
-sudo mv /home/box/web/public/hello.py /home/box/etc/
+sudo mv ~/web/public/hello.py /home/box/etc
 sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo gunicorn -b 0.0.0.0:8080 hello:app &
--- public/hello.py
+++ public/hello.py
@@ -1,10 +1,5 @@
-CONFIG = {
-	'mode': 'wsgi',
-	'working_dir': '/home/box/web',
-	'python': 'usr/bin/python3',
-	'args': (
-	'--bind=0.0.0.0:8080',
-	'--workers=4'
-	'hello:app',
-	),
-}
+bind "0.0.0.0:8080"
+working_dir: /home/box/web
+hello:app
+workers = 4
+python = /usr/bin/python3
