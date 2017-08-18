ffe018699b5626a55d7ccec95581961bedf5a8d8
ForStepik <e.ts.14@mail.ru>
2017-08-18 15:25:51 +0300

    i

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def app(environ, start_response):
 	status = '200 OK'
 	header = [('Content-type', 'text-plain')]
-	body = "\n".join(environ['QUERY_STRING'].split('&'))
+	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING].split('&')]
 	start_response(status, header)
 	return body
