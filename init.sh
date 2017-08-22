f4c77164871ae615a0623f5820e093a94fef6ae0
ForStepik <e.ts.14@mail.ru>
2017-08-22 21:53:07 +0300

    1.9.11

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
+cd ~/web
+mkdir /home/box/etc
+sudo mv ~/web/public/hello.py /home/box/etc/
+sudo -s ~/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo gunicorn -c /home/box/web/etc/hello.py hello:wsgi:application
+sudo ginicorn -b 0.0.0.0:8080 hello:app &
 sudo /etc/init.d/nginx start
--- public/hello.py
+++ public/hello.py
@@ -1,5 +1,5 @@
-bind "0.0.0.0:8080"
-pythonpath: /home/box/web
+bind '0.0.0.0:8080'
+working_dir: /home/box/web
 python: /usr/bin/python3
 hello:app
-workers = 4
+workers = 5
