c3ffae2db94693e0502e5ecb97eeec063a6c0746
ForStepik <e.ts.14@mail.ru>
2017-08-31 21:33:54 +0300

    1.9.11

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 cd ~/web
+mkdir ~/etc
 mv ~/web/etc/hello.py /home/box/etc/ 
 sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
