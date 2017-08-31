49d0bcb580143bcc7cb2f2dd2078d62c0a7b69ff
ForStepik <e.ts.14@mail.ru>
2017-08-31 21:07:17 +0300

    1.9.11

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app
 sudo /etc/init.d/nginx start
--- public/hello.py
+++
@@ -1,5 +0,0 @@
-bind '0.0.0.0:8080'
-working_dir: /home/box/web
-python: /usr/bin/python3
-hello:app
-workers = 5
